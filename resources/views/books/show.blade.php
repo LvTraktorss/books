beb1917495068a76cbcd3d49746faddfa7c8900f
dom <dominiksvecbastiks@Opti-11.localdomain>
2026-02-26 11:38:51 +0200

    pievienoju create

---
+++ resources/views/books/show.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" action="/books/create">
+    @scrf
+
+    <label for="title">Title</label><br>
+    <input type="text" name="title" id="title">
+
+    <label for="author">Author</label><br>
+    <input type="text" name="author" id="title">
+
+    
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BooksController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/books/create',[BookController::class,'create']);
